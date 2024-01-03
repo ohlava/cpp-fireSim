c95a5aab5a1e5067cc2ebc997e5f05b88b71584e
Ondřej Hlava <hlava@OHMacBook.local>
2024-01-03 19:06:54 +0100

    SFML sleeping optimalization

--- project/fireSimulator/main.cpp
+++ project/fireSimulator/main.cpp
@@ -1,10 +1,13 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/System.hpp> // Include for sf::sleep
+
+#include <iostream>
 #include <vector>
 
 // Function prototypes
 void initializeButtons(std::vector<sf::RectangleShape>& buttons, int tileSize);
 void initializeTiles(std::vector<sf::RectangleShape>& tiles, int windowHeight);
-void handleEvents(sf::RenderWindow& window, std::vector<sf::RectangleShape>& buttons, std::vector<sf::RectangleShape>& tiles);
+bool handleEvents(sf::RenderWindow& window, std::vector<sf::RectangleShape>& buttons, std::vector<sf::RectangleShape>& tiles);
 void drawElements(sf::RenderWindow& window, const std::vector<sf::RectangleShape>& buttons, const std::vector<sf::RectangleShape>& tiles);
 
 const int NUM_TILES_PER_AXIS = 20;
@@ -19,9 +22,24 @@ int main() {
     std::vector<sf::RectangleShape> tiles;
     initializeTiles(tiles, window.getSize().y);
 
+    sf::Clock clock; // Clock to measure idle time
+    drawElements(window, buttons, tiles);
+
     while (window.isOpen()) {
-        handleEvents(window, buttons, tiles);
-        drawElements(window, buttons, tiles);
+
+        if (handleEvents(window, buttons, tiles)) {
+            drawElements(window, buttons, tiles);
+            clock.restart();
+        }
+
+        if (!window.hasFocus()) {
+            sf::sleep(sf::milliseconds(80));         // Sleep to reduce CPU usage
+        }
+
+        if (clock.getElapsedTime().asSeconds() > 1) {
+            // If idle for more than 5 seconds, reduce the frequency of event polling
+            sf::sleep(sf::milliseconds(50));
+        }
     }
 
     return 0;
@@ -64,7 +82,11 @@ void initializeTiles(std::vector<sf::RectangleShape>& tiles, int windowHeight) {
     }
 }
 
-void handleEvents(sf::RenderWindow& window, std::vector<sf::RectangleShape>& buttons, std::vector<sf::RectangleShape>& tiles) {
+// Returns true if redraw of window is needed based on user actions
+bool handleEvents(sf::RenderWindow& window, std::vector<sf::RectangleShape>& buttons, std::vector<sf::RectangleShape>& tiles) {
+
+    // TODO make reequest for redrawing more sophisticated - based on also the simulation updates
+    bool needRedraw = false;
     sf::Event event;
     sf::Vector2i mousePos = sf::Mouse::getPosition(window);
 
@@ -79,19 +101,30 @@ void handleEvents(sf::RenderWindow& window, std::vector<sf::RectangleShape>& but
                     btn.setFillColor(sf::Color::Red);
 
                     // TODO Add button functionality here
+
+                    needRedraw = true;
                 }
             }
         }
     }
 
+    // TODO remember last tile, only one can be highlighted at any given time, so we dont need to iterate through all tiles
     // Change color on tile hover
     for (auto& tile : tiles) {
         if (tile.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
-            tile.setFillColor(sf::Color::Yellow);
+            if (tile.getFillColor() != sf::Color::Yellow) {
+                tile.setFillColor(sf::Color::Yellow);
+                needRedraw = true;
+            }
         } else {
-            tile.setFillColor(sf::Color::White); // Revert color
+            if (tile.getFillColor() != sf::Color::White) {
+                tile.setFillColor(sf::Color::White); // Revert color
+                needRedraw = true;
+            }
         }
     }
+
+    return needRedraw;
 }
 
 void drawElements(sf::RenderWindow& window, const std::vector<sf::RectangleShape>& buttons, const std::vector<sf::RectangleShape>& tiles) {
